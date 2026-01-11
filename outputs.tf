63761af9ce39140226a617aafd3a2b6ec7666ddd
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:50:00 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "app_configuration_keys" {
+  description = "All app_configuration_key resources"
+  value       = azurerm_app_configuration_key.app_configuration_keys
+}
+
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "app_configuration_keys" {
+  description = <<EOT
+Map of app_configuration_keys, attributes below
+Required:
+    - configuration_store_id
+    - key
+Optional:
+    - content_type
+    - etag
+    - label
+    - locked
+    - tags
+    - type
+    - value
+    - vault_key_reference
+EOT
+
+  type = map(object({
+    configuration_store_id = string
+    key                    = string
+    content_type           = optional(string)
+    etag                   = optional(string)
+    label                  = optional(string)
+    locked                 = optional(bool, false)
+    tags                   = optional(map(string))
+    type                   = optional(string, "kv")
+    value                  = optional(string)
+    vault_key_reference    = optional(string)
+  }))
+}
+

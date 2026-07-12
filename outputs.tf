d0e7139d8d51d10d32207ef523ec7f110ddb3d55
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "app_configuration_keys" {
-  description = "All app_configuration_key resources"
-  value       = azurerm_app_configuration_key.app_configuration_keys
-}
 output "app_configuration_keys_configuration_store_id" {
-  description = "List of configuration_store_id values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.configuration_store_id]
+  description = "Map of configuration_store_id values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.configuration_store_id }
 }
 output "app_configuration_keys_content_type" {
-  description = "List of content_type values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.content_type]
+  description = "Map of content_type values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.content_type }
 }
 output "app_configuration_keys_etag" {
-  description = "List of etag values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.etag]
+  description = "Map of etag values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.etag }
 }
 output "app_configuration_keys_key" {
-  description = "List of key values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.key]
+  description = "Map of key values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.key }
 }
 output "app_configuration_keys_label" {
-  description = "List of label values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.label]
+  description = "Map of label values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.label }
 }
 output "app_configuration_keys_locked" {
-  description = "List of locked values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.locked]
+  description = "Map of locked values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.locked }
 }
 output "app_configuration_keys_tags" {
-  description = "List of tags values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.tags]
+  description = "Map of tags values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.tags }
 }
 output "app_configuration_keys_type" {
-  description = "List of type values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.type]
+  description = "Map of type values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.type }
 }
 output "app_configuration_keys_value" {
-  description = "List of value values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.value]
+  description = "Map of value values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.value }
 }
 output "app_configuration_keys_vault_key_reference" {
-  description = "List of vault_key_reference values across all app_configuration_keys"
-  value       = [for k, v in azurerm_app_configuration_key.app_configuration_keys : v.vault_key_reference]
+  description = "Map of vault_key_reference values across all app_configuration_keys, keyed the same as var.app_configuration_keys"
+  value       = { for k, v in azurerm_app_configuration_key.app_configuration_keys : k => v.vault_key_reference }
 }
 
